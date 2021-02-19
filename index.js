81ea6ff982a90a0843f53c50059acbcaa112e74c
abhimanyu_mtx <abhimanyu.tanwar@mtxb2b.com>
2021-02-19 16:28:08 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var pg  = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/salesforce';
+if (process.env.DATABASE_URL !== undefined) {
+  pg.defaults.ssl = true;
+}
+var client = new pg.Client(connectionString);
+client.connect();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  client.query('SELECT * FROM salesforce.Account', function(error, data) {
+    response.json(data.rows);
+  });  
 })
 
 app.get('/multiply/:num1/:num2', function(request, response) {
